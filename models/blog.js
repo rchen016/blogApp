e192473b8a227711ea3265fe8843c6cd459f7dc1
Chen <rchen@CompanyNet.org>
2019-06-18 15:36:10 -0700

    switching to bootstrap

--- models/blog.js
+++ models/blog.js
@@ -3,7 +3,9 @@ var mongoose = require("mongoose");
 var BlogSchema = new mongoose.Schema({
 	title: String,
 	image: String,
+	showImage: String,
 	body: String,
+	date: String,
 	created: {type: Date, default: Date.now}
 });
 
